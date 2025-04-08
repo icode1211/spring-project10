d751ff5761fe8f51332539f7b6eb194b13a2609f
icode <icode1211@naver.com>
2025-04-08 15:16:33 +0900

    Spring 5일차(6) - Blog 게시글 수정 실패시 400 HTTP Status Code 테스트

--- src/test/java/com/estsoft/demo/blog/controller/BlogControllerTest.java
+++ src/test/java/com/estsoft/demo/blog/controller/BlogControllerTest.java
@@ -6,6 +6,7 @@ import com.estsoft.demo.blog.dto.UpdateArticleRequest;
 import com.estsoft.demo.blog.repository.BlogRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -135,5 +136,22 @@ class BlogControllerTest {
         Article article = blogRepository.findById(id).orElseThrow();
         assertThat(article.getTitle()).isEqualTo(request.getTitle());
         assertThat(article.getContent()).isEqualTo(request.getContent());
+    }
+
+    @DisplayName("Exception 발생시 400 Status Code 검증하는 테스트 코드")
+    @Test
+    public void updateArticleFailed() throws Exception {
+        // given: 존재하지 않는 ID 임의로 세팅
+        Long noExistsId = 1000L;
+        UpdateArticleRequest request = new UpdateArticleRequest("수정할title", "수정할content");
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        // when: 게시글 수정API 호출
+        ResultActions resultActions = mockMvc.perform(put("/api/articles/{id}", noExistsId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody));
+
+        // then:
+        resultActions.andExpect(status().isBadRequest());
     }
 }
