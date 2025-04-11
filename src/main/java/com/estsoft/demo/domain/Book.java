86a9284b5a5f0f2000b7b3ac7ef17835125180e5
icode <icode1211@naver.com>
2025-04-11 13:33:13 +0900

    Spring 8일차(2) - 책정보 조회/저장 view controller

--- src/main/java/com/estsoft/demo/domain/Book.java
+++ src/main/java/com/estsoft/demo/domain/Book.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @Entity
 public class Book {
     @Id
