af32101ccd8a5079a6e571dafc131490a4ed51b1
icode <icode1211@naver.com>
2025-04-14 15:06:44 +0900

    Spring 9일차(3) - AOP 적용 - 메소드 호출 시간 로깅 Aspect

---
+++ src/main/java/com/estsoft/demo/aop/TimeLoggingAspect.java
@@ -0,0 +1,31 @@
+package com.estsoft.demo.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class TimeLoggingAspect {
+    /* Pointcut */
+    @Pointcut("execution(* com.estsoft.demo.blog.service..*(..))")
+    public void pointcut() {}
+
+    /* Advice */
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) {
+        long startTimeMs = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throw new RuntimeException(throwable);
+        } finally {
+            long endTimeMs = System.currentTimeMillis();
+            log.info("메소드 실행 시간: {}ms", endTimeMs - startTimeMs);
+        }
+    }
+}
